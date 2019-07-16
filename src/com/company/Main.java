4dc470e9fa2a1b237251e6fe440a8d0084d7cdbe
wilkinsonbrian <wilkibrian@gmail.com>
2019-07-16 12:13:35 -0400

    Added a scanner and updated print statement.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, World.");
+        Scanner scan = new Scanner(System.in);
+        String myName = scan.nextLine();
+
+        System.out.println("Hello, World." + myName);
     }
 }
